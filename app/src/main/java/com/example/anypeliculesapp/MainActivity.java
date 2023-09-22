25d285aedc6992a8c8158103cd92a5f6e284801c
Zoe <a23zoeguepar@inspedralbes.cat>
2023-09-22 13:24:35 +0200

    Crear pregunta method a medias

--- app/src/main/java/com/example/anypeliculesapp/MainActivity.java
+++ app/src/main/java/com/example/anypeliculesapp/MainActivity.java
@@ -5,6 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +20,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String[] respostes = new String[4];
+        respostes[0] = "2022";
+        respostes[1] = "2004";
+        respostes[2] = "2022";
+        respostes[3] = "2004";
+        crearPregunta("Prueba", respostes);
         Button button = findViewById(R.id.testButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,4 +35,27 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void crearPregunta(String nomPeli, String[] respostestext){
+        LinearLayout layout = findViewById(R.id.mainlayout);
+
+        TextView peli= new TextView(this);
+        peli.setText(nomPeli);
+        peli.setTextSize(34);
+        peli.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        layout.addView(peli);
+
+        RadioGroup radiogroup = new RadioGroup(this);
+        RadioButton[] respuestas = new RadioButton[4];
+        for(int i =0; i<respostestext.length; i++) {
+            respuestas[i] = new RadioButton(this);
+            respuestas[i].setText(respostestext[i]);
+            respuestas[i].setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            radiogroup.addView(respuestas[i]);
+        }
+        layout.addView(radiogroup);
+
+    }
+
+
 }
+
