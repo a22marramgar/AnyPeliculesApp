621d281b07f1d9ddbbf1f10972d82a0a1eca2eb2
Zoe <a23zoeguepar@inspedralbes.cat>
2023-09-26 11:00:46 +0200

    crear pregunta method working

--- app/src/main/java/com/example/anypeliculesapp/MainActivity.java
+++ app/src/main/java/com/example/anypeliculesapp/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.anypeliculesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.OrientationHelper;
 
+import android.app.ActionBar;
+import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
-
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,39 +25,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String[] respostes = new String[4];
-        respostes[0] = "2022";
+        respostes[0] = "2002";
         respostes[1] = "2004";
-        respostes[2] = "2022";
+        respostes[2] = "2012";
         respostes[3] = "2004";
-        crearPregunta("Prueba", respostes);
-        Button button = findViewById(R.id.testButton);
+        crearPregunta("Avatar", respostes);
+        crearPregunta("Prueba2", respostes);
+        Button button = findViewById(R.id.sendButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                button.setText("Holaa");
+
+                // to get Context
+                Context context = getApplicationContext();
+                // message to display
+                String text = "Toast message";
+                // toast time duration, can also set manual value
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                // to show the toast
+                toast.show();
             }
         });
     }
 
     public void crearPregunta(String nomPeli, String[] respostestext){
-        LinearLayout layout = findViewById(R.id.mainlayout);
+        LinearLayout layout = findViewById(R.id.linearlayout);
 
         TextView peli= new TextView(this);
         peli.setText(nomPeli);
         peli.setTextSize(34);
         peli.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
         layout.addView(peli);
-
         RadioGroup radiogroup = new RadioGroup(this);
-        RadioButton[] respuestas = new RadioButton[4];
+        layout.addView(radiogroup);
+        radiogroup.setGravity(Gravity.CENTER);
+        RadioButton[] respuestas = new RadioButton[respostestext.length];
         for(int i =0; i<respostestext.length; i++) {
             respuestas[i] = new RadioButton(this);
             respuestas[i].setText(respostestext[i]);
-            respuestas[i].setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            respuestas[i].setTextSize(20);
+            respuestas[i].setTextAlignment(View.TEXT_ALIGNMENT_GRAVITY);
             radiogroup.addView(respuestas[i]);
-        }
-        layout.addView(radiogroup);
 
+        }
     }
 
 
